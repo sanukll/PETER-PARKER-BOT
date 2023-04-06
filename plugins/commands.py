2fcd55c4dcb8a6a7bc8af270b9ac1bfbd1336e42
sanukll <100078539+sanukll@users.noreply.github.com>
2023-04-06 08:31:10 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -38,17 +38,17 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention, message.from_user.username, temp.U_NAME))
     if len(message.command) != 2:
         buttons =  [[
-            InlineKeyboardButton('⚚ ΛᎠᎠ MΞ ϮԾ YԾUᏒ GᏒԾUᎮ ⚚', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('✚ 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 ✚', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
 
         ],  [
-             InlineKeyboardButton('DISCLAIMER', callback_data='disclaimer')
+             InlineKeyboardButton('✆ 𝙳𝙸𝚂𝙲𝙻𝙰𝙸𝙼𝙴𝚁', callback_data='disclaimer')
         ],  [
-             InlineKeyboardButton('CHANNELS & GROUPS ', callback_data='channels')
+             InlineKeyboardButton('➣ 𝙾𝚄𝚁 𝙲𝙷𝙰𝙽𝙽𝙴𝙻𝚂 & 𝙶𝚁𝙾𝚄𝙿𝚂', callback_data='channels')
         ],  [
 
-            InlineKeyboardButton(' HΞLᎮ ', callback_data='help'),
+            InlineKeyboardButton(' ☛ 𝙷𝙴𝙻𝙿 ☚ ', callback_data='help'),
 
-            InlineKeyboardButton(' ΛBOUT ', callback_data='about')
+            InlineKeyboardButton(' ☛ 𝙰𝙱𝙾𝚄𝚃 ☚ ', callback_data='about')
 
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
@@ -93,17 +93,17 @@ async def start(client, message):
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons =  [[
-            InlineKeyboardButton('⚚ ΛᎠᎠ MΞ ϮԾ YԾUᏒ GᏒԾUᎮ ⚚', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('✚ 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 ✚', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
 
         ],  [
-             InlineKeyboardButton('DISCLAIMER', callback_data='disclaimer')
+             InlineKeyboardButton('✆ 𝙳𝙸𝚂𝙲𝙻𝙰𝙸𝙼𝙴𝚁', callback_data='disclaimer')
         ],  [
-             InlineKeyboardButton('CHANNELS & GROUPS ', callback_data='channels')
+             InlineKeyboardButton('➣ 𝙾𝚄𝚁 𝙲𝙷𝙰𝙽𝙽𝙴𝙻𝚂 & 𝙶𝚁𝙾𝚄𝙿𝚂', callback_data='channels')
         ],  [
 
-            InlineKeyboardButton(' HΞLᎮ ', callback_data='help'),
+            InlineKeyboardButton(' ☛ 𝙷𝙴𝙻𝙿 ☚ ', callback_data='help'),
 
-            InlineKeyboardButton(' ΛBOUT ', callback_data='about')
+            InlineKeyboardButton(' ☛ 𝙰𝙱𝙾𝚄𝚃 ☚ ', callback_data='about')
 
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
